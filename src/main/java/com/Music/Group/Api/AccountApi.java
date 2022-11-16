e5d941e90a36d2d927d0954774a507987dabf807
eun091696 <eun091696@gmail.com>
2022-11-16 17:43:42 +0900

    11.16 register 화면 진행 예정

---
+++ src/main/java/com/Music/Group/Api/AccountApi.java
@@ -0,0 +1,11 @@
+package com.Music.Group.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountApi {
+
+
+}
