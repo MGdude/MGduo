38c5f4935b868344312f68c286febc037bc1a311
eun091696 <eun091696@gmail.com>
2022-11-17 13:54:22 +0900

    11.17 백업용

--- src/main/resources/static/js/account/register.js
+++ src/main/resources/static/js/account/register.js
@@ -1,1 +1,12 @@
-class 
+const registerBtn = document.querySelector(".regist");
+
+registerBtn.onclick = () => {
+    const accountInputs = document.querySelectorAll(".login-content input");
+    
+    let user = {
+        userName: accountInputs[0].value,
+        password: accountInputs[1].value
+    }
+
+    console.log(user);
+}
