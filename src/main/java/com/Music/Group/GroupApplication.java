e73209936ca289d280938e9a0b529d892839ac9c
eun091696 <eun091696@gmail.com>
2022-11-24 14:40:06 +0900

    11.24 댓글 입력 시 db에 전송 완료(유저 아이디 로그인 아이디로 변경 필요)

--- src/main/java/com/Music/Group/GroupApplication.java
+++ src/main/java/com/Music/Group/GroupApplication.java
@@ -1,13 +1,4 @@
 package com.Music.Group;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class GroupApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GroupApplication.class, args);
-	}
-
+public class GruopApplication {
 }
