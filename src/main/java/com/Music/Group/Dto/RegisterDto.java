6c77949b960408c78cf3838f3f8a7d8108b3300a
eun091696 <eun091696@naver.com>
2022-12-12 16:24:08 +0900

    no message

---
+++ src/main/java/com/Music/Group/Dto/RegisterDto.java
@@ -0,0 +1,35 @@
+package com.Music.Group.Dto;
+
+import com.Music.Group.Domain.User;
+import com.Music.Group.Validation.ValidationGroup;
+import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class RegisterDto {
+    @NotBlank(message = "아이디는 비워둘 수 없습니다.", groups = ValidationGroup.NotBlankGroup.class)
+    @Size(min = 3, message = "아이디는 3글자 이상 입력 가능합니다.", groups = ValidationGroup.SizedGroup.class)
+    @Pattern(regexp = "^[a-zA-Z\\d]*$", message = "아이디는 영어, 숫자만 입력 가능합니다.", groups = ValidationGroup.PatternCheckGroup.class)
+    private String username;
+
+    @NotBlank(message = "비밀번호는 비워둘 수 없습니다.1", groups = ValidationGroup.NotBlankGroup.class)
+    @Size(min = 8, max = 16, message = "최소 8자 이상, 최대 16자 이하로 입력해주세요", groups = ValidationGroup.SizedGroup.class)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]*$", message = "비밀번호는 특수기호, 영문, 숫자를 모두 포함해야합니다.", groups = ValidationGroup.PatternCheckGroup.class)
+    private String password;
+
+    @NotBlank(message = "비밀번호는 비워둘 수 없습니다.2", groups = ValidationGroup.NotBlankGroup.class)
+    @Size(min = 8, max = 16, message = "최소 8자 이상, 최대 16자 이하로 입력해주세요", groups = ValidationGroup.SizedGroup.class)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]*$", message = "비밀번호는 특수기호, 영문, 숫자를 모두 포함해야합니다.", groups = ValidationGroup.PatternCheckGroup.class)
+    private String passwordChk;
+
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .password(new BCryptPasswordEncoder().encode(password))
+                .build();
+    }
+}
